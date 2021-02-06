454ccaedc09ae3279c8b583e9e8f8d82b4f2d587
Devgon1 <grc74@hotmail.com>
2021-02-06 23:11:15 +0100

    styles finished

--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,8 @@
 const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const fadeElems = document.querySelectorAll(".has-fade");
-const body = document.querySelector("body");
 
 btnHamburger.addEventListener("click", function () {
   console.log("open hamburger");
